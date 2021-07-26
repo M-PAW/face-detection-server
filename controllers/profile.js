02b52d63a8f88447cbaedc11accc6fd5327b0cb5
Michael Walter <michael_patrick.walter@outlook.com>
2021-07-26 16:56:26 -0700

    Updates: Finished updating helper functions/controllers for endpoints. Redis sessions should be ready to test soon.

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,7 +1,7 @@
+//const { response } = require('express');
 
-const handleProfileGet = (req,res, db) => {
+const handleProfileGet = (req, res, db) => {
     const { id } = req.params;
-    
     db
      .select('*').from('users').where({id})
      .then( user => {
@@ -14,6 +14,26 @@ const handleProfileGet = (req,res, db) => {
      .catch(err => res.status(400).json('Error getting user'))
  }
 
+ const handleProfileUpdate = (req, res, db) => {
+    const { id } = req.params;
+    const { name, age, about } = req.body.formInput;
+
+    db('users')
+        .where({ id })
+        .update({ name, age, about })
+        .then( response => {
+            if (response) {
+                res.status(201).json('Success')
+            } else {
+                res.status(400).json('Failure')
+            }
+        })
+        .catch(err => {
+            res.status(400).json('Error updating user data')
+        })
+ }
+
  module.exports = {
-     handleProfileGet
+     handleProfileGet,
+     handleProfileUpdate
  }
--- postgres/tables/users.sql
+++ postgres/tables/users.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
     id serial PRIMARY KEY,
     name VARCHAR(100),
     age INT DEFAULT 0,
-    pet text DEFAULT '',
+    about text DEFAULT '',
     email text UNIQUE NOT NULL,
     entries BIGINT DEFAULT 0,
     joined TIMESTAMP NOT NULL
